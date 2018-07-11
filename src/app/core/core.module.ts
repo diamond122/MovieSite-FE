c3fe12243810f273dc2238aa89082f16076a271f
diamond-shine <focusingdev@outlook.com>
2018-07-11 01:36:16 -0500

    routing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AppRoutingModule } from '../app.routing';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   declarations: [SideBarComponent, NavBarComponent],
   exports: [SideBarComponent, NavBarComponent]
